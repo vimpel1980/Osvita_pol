462b5bfff97a2a50313188b27181098dc2355478
Vimpel <vimpel28@gmail.com>
2016-06-15 15:25:50 +0300

    add setting gulp and add dev

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp=require('gulp');
+var includer = require('gulp-htmlincluder');
+var connect = require('gulp-connect');
+var livereload = require('gulp-livereload');
+var spritecreat = require('gulp.spritesmith')
+var less = require('gulp-less')
+
+gulp.task('sprite', function(){
+    var spriteData = gulp.src('dev/img/icons/*.png').pipe(spritecreat({
+        imgName: 'sprite.png',
+        cssName: 'sprite.css',
+        algorithm: 'binary-tree'
+    }));
+    spriteData.img.pipe(gulp.dest('build/img/'));
+    spriteData.css.pipe(gulp.dest('dev/css'));
+});
+ 
+gulp.task('html', function(){
+	gulp.surc('dev/**/*.html')
+		.pipe(includer())
+    .pipe(gulp.dest('build/'))
+    .pipe(connect.reload());
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'build',
+    livereload: true
+  });
+});
+
+gulp.task('less', function(){
+  gulp.src('general.less')
+  .pipe(less())
+  .pipe(gulp.dest('build/css/'));
+});
+
+gulp.task('default', function(){
+  gulp.start('concat', 'html', 'server', 'less');
+  gulp.watch(['dev/css/**/*.css'], function(){
+    gulp.start('concat');})
+  gulp.watch(['dev/**/*.html'], function(){
+    gulp.start('html');})
+  gulp.watch(['dev/**/*.less'], function(){
+    gulp.start('less');})
+})
